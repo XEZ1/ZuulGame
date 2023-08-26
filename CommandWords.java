0ff0278380e71073a0fbb93e0a9108315b0e788a
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-08-26 19:49:17 +0100

    Add the CommandWords class

---
+++ CommandWords.java
@@ -0,0 +1,49 @@
+/**
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.
+ * 
+ * This class holds an enumeration of all command words known to the game.
+ * It is used to recognise commands as they are typed in.
+ *
+ */
+
+public class CommandWords
+{
+    // a constant array that holds all valid command words
+    private static final String[] validCommands = {
+        "go", "quit", "help", "investigate", "pick", "list_items", "get_the_overall_weight", "back", "investigate_where_are_the_characters", "give"
+    };
+
+    /**
+     * Constructor - initialise the command words.
+     */
+    public CommandWords()
+    {
+        // nothing to do at the moment...
+    }
+
+    /**
+     * Check whether a given String is a valid command word. 
+     * @param aString String command.
+     * @return true if it is, false if it isn't.
+     */
+    public boolean isCommand(String aString)
+    {
+        for(int i = 0; i < validCommands.length; i++) {
+            if(validCommands[i].equals(aString))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Print all valid commands to System.out.
+     */
+    public void showAll() 
+    {
+        for(String command: validCommands) {
+            System.out.print(command + "  ");
+        }
+        System.out.println();
+    }
+}
